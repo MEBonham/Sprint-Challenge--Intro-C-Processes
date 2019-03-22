b4b7587512f9622f2b376dcc5ffa9aa218e2c124
MEBonham <programmer@mckaybonham.com>
2019-03-22 10:05:30 -0600

    Should be working but gives seg-fault

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <dirent.h>
+#include <sys/stat.h>
 
 /**
  * Main
@@ -7,12 +8,28 @@
 int main(int argc, char **argv)
 {
   // Parse command line
+  char *path = ".";
+  if (argc > 0)
+  {
+    path = argv[0];
+  }
 
   // Open directory
+  DIR *dir_ptr = opendir(path);
 
   // Repeatly read and print entries
+  struct dirent *file_info;
+  char *file_name;
+  struct stat size_buf;
+  while ((file_info = readdir(dir_ptr)) != NULL)
+  {
+    file_name = file_info->d_name;
+    stat(file_name, &size_buf);
+    printf("%10lld  %s\n", (long long int) size_buf.st_size, file_name);
+  }
 
   // Close directory
+  closedir(dir_ptr);
 
   return 0;
 }
